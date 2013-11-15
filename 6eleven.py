729337c90e7a7a3b57bdfe33a8fc2bc91007d718
Jaime Yu <jaime@jaimeyu.com>
2013-11-14 22:00:27 -0500

    Initial working prototype.

---
+++ 6eleven.py
@@ -0,0 +1,41 @@
+def findSubWordsIn(word):
+    print "Attempting to find subwords for " + word.strip() + "."
+    qSubWords = []
+    words = open("list.txt", "r") #readonly
+    for candidate in words:
+        candidate = candidate.strip()
+        #print "Checking if " + candidate + " is a substring of " + word
+        if -1 != word.find(candidate,0,len(word)):
+            if candidate != word:
+                #print "Found candidate: " + candidate
+                qSubWords.append(candidate)
+    return qSubWords
+
+
+words = open("list.txt", "r") #readonly
+
+qWords = []
+
+
+for word in words:
+    word = word.strip()
+
+    #optimization.
+    if len(word) < 6:
+        continue
+    subWords = []
+    subWords = findSubWordsIn(word)
+    #subWords = findSubWordsIn("abbas")
+    if ( len(subWords) > 10):
+        collection = []
+        collection.append(word)
+        collection.append(subWords)
+        qWords.append(collection)
+
+#print qWords
+for item in qWords:
+    print "*************************************"
+    print "'" + item[0] +"' has the following subwords:"
+    print item[1]
+
+
